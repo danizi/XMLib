60046e10492cc8dc0c0fb05a14b0c51ddd6dacfc
xiaomin <15074770708@163.com>
2019-05-14 19:16:49 +0800

    commit

--- LIB_Common/src/main/java/com/xm/lib/common/util/NumUtil.kt
+++ LIB_Common/src/main/java/com/xm/lib/common/util/NumUtil.kt
@@ -8,7 +8,7 @@ class NumUtil {
          * 带小数点的数值
          */
         fun getDecimalPoint(obj: Any): String {
-            return DecimalFormat("0.00").format(obj)
+            return DecimalFormat("00.00").format(obj)
         }
     }
 
