4dc7a54b3111c5105427e113e71239f6d6bce177
xm <15074770708@163.com>
2019-05-19 20:09:10 +0800

    commit base

--- LIB_Common/src/main/java/com/xm/lib/common/base/rv/BaseRvAdapter.kt
+++ LIB_Common/src/main/java/com/xm/lib/common/base/rv/BaseRvAdapter.kt
@@ -9,35 +9,61 @@ import android.view.ViewGroup
  * RecyclerView 适配器
  */
 abstract class BaseRvAdapter : RecyclerView.Adapter<BaseViewHolder>() {
+    /**
+     * 数据集合
+     */
     var data: ArrayList<Any>? = ArrayList()
+    /**
+     * 不同类型ViewHolder管理
+     */
     private var delegateManager: DelegateManager? = null
+    /**
+     * 过时了
+     */
+    @Deprecated("不要在使用")
     private var onBindDataListener: OnBindDataListener? = null
 
     init {
         delegateManager = DelegateManager()
     }
 
+    /**
+     * 创建ViewHolder
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-        val delegateBean: DelegateBean = delegateManager?.targets?.get(viewType)!!
+        //根据回传的viewType获取指定ViewHolder信息
+        val delegateBean: DelegateBean = delegateManager?.targets?.get(viewType)!!  //ps:需要处理一下这里课程会出问题
+        //获取ViewHolder View实例
         val v = LayoutInflater.from(parent.context).inflate(delegateBean.itemViewLayoutId!!, parent, false)
+        //创建ViewHolder实例
         val viewHolder: BaseViewHolder? = delegateBean.viewHolderClass.getConstructor(View::class.java).newInstance(v) as BaseViewHolder?
+        //保存ViewHolder实例
+        delegateManager?.typeViewHolders?.add(viewHolder!!)
         return viewHolder!!
     }
 
+    /**
+     * item界面返回的类型
+     */
     override fun getItemViewType(position: Int): Int {
         for (d in delegateManager?.targets?.entries!!) {
-            if (d.value.beanClass == data!![position]!!.javaClass) {
+            if (d.value.beanClass == data!![position].javaClass) {
                 return d.key
             }
         }
         return super.getItemViewType(position)
     }
 
+    /**
+     * 数据数量
+     */
     override fun getItemCount(): Int {
         return if (data?.isEmpty()!!) 0 else data?.size!!
     }
 
-
+    /**
+     * 绑定（界面-数据）
+     */
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         bindVH(holder, position)
     }
@@ -46,30 +72,36 @@ abstract class BaseRvAdapter : RecyclerView.Adapter<BaseViewHolder>() {
         /*绑定数据*/
         if (data?.get(position) != null) {
             holder.bindData(data!![position]!!, position)
+            holder.bindData(this, data!![position], position)
             onBindDataListener?.onBindData(holder.itemView, data!![position]!!, position)
         }
     }
 
+    /**
+     * 委派者添加需要被委派任务的ViewHolder
+     */
     fun addItemViewDelegate(viewType: Int, viewHolderClass: Class<*>, BeanClass: Class<*>, itemViewLayoutId: Int?) {
-        /*委派者添加需要被委派任务的viewHolder*/
-        delegateManager?.targets?.put(viewType, DelegateBean(viewHolderClass, BeanClass,itemViewLayoutId))
+        delegateManager?.targets?.put(viewType, DelegateBean(viewHolderClass, BeanClass, itemViewLayoutId))
     }
 
-//    fun setOnBindDataListener(listener: OnBindDataListener) {
-//        /*itemView绑定监听,处理ViewHolder,对itemView进行绑定操作*/
-//        onBindDataListener = listener
-//    }
+    /**
+     * 获取ViewHolder实例
+     */
+    fun getTypeViewHolder(type: Int): BaseViewHolder {
+        return delegateManager?.typeViewHolders?.get(type)!!
+    }
 
     /**
-     * 委派者
+     * item界面委派者
      */
     private class DelegateManager {
         val targets = HashMap<Int, DelegateBean>()
+        val typeViewHolders = ArrayList<BaseViewHolder>()
     }
 
     /**
-     * 委派者实体
+     * item界面委派者实体
      */
-    private class DelegateBean(val viewHolderClass: Class<*>, val beanClass: Class<*>,val itemViewLayoutId: Int?)
+    private class DelegateBean(val viewHolderClass: Class<*>, val beanClass: Class<*>, val itemViewLayoutId: Int?)
 }
 
--- LIB_Common/src/main/java/com/xm/lib/common/base/rv/BaseViewHolder.kt
+++ LIB_Common/src/main/java/com/xm/lib/common/base/rv/BaseViewHolder.kt
@@ -7,9 +7,28 @@ import android.view.View
 
 abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val views: SparseArray<View>? = null
+    @Deprecated("不要使用")
     private var listener: OnBindDataListener? = null
+
+    /**
+     * 绑定数据
+     * @param d 绑定的数据
+     * @param position 绑定的数据下标位置
+     */
     abstract fun bindData(d: Any, position: Int)
 
+    /**
+     * 当使用嵌套的时候可以使用
+     * @param adapter 适配器
+     * @param d 绑定的数据
+     * @param position 绑定的数据下标位置
+     */
+    open fun bindData(adapter: BaseRvAdapter, d: Any, position: Int) {}
+
+    /**
+     * 查找控件id
+     */
+    @Deprecated("使用了https://www.buzzingandroid.com/tools/android-layout-finder/来替代了")
     fun <T : View> getView(viewId: Int): T? {
         /*获取view*/
         var view = views?.get(viewId)
@@ -22,6 +41,7 @@ abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 }
 
 interface OnBindDataListener {
+    @Deprecated("不要使用")
     fun onBindData(v: View?, d: Any, position: Int)
 }
 
