1beae951374ab60e4c269880ce1ac87138612154
xm <15074770708@163.com>
2019-09-05 01:00:29 +0800

    commit base

--- app/src/test/java/com/xm/lib/test/ExampleUnitTest.kt
+++ app/src/test/java/com/xm/lib/test/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.xm.lib.test
 
-import org.junit.Test
+
 
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,7 +14,7 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, (2 + 2).toLong())
-
-
     }
+
+
 }
